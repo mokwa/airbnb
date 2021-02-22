4205d27566d7562493ecb799f68f270621676caa
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-02-22 17:42:00 +0000

    added fields to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  # def configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  # end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
