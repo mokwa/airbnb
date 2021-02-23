eed9271e1bc8b37c21745512b6a6ef03a46f3d10
Tina Iuso <tinaiuso@gmail.com>
2021-02-23 18:07:10 +0000

    reviews controller to fix

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+
+    def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+    end
+
+    def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @flat = Flat.find(params[:flat_id])
+    @review.user = current_user
+
+    if @review.save
+      redirect_to flats_path(@flat)
+    else
+      render :new
+    end
+        
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :description)
+    end
+end
