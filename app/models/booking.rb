3bf88bad31c4f30b88fb1ceab77236ccb70d0b85
Juliana Mokwa <julimokwa@gmail.com>
2021-02-23 16:22:31 +0000

    bookings controller and routes added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,9 @@
 class Flat < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
