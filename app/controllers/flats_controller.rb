3959bdfb64a81f1e3841fb49aeac44aa631e9183
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-02-23 15:16:30 +0000

    added flats views and controllers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,44 +1,43 @@
 class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index] 
 
-  skip_before_action :authenticate_user!, only: :index
-
   def index
     @flats = Flat.all
   end
 
   def show
-
     find_flat
   end
 
   def new
      @flat = Flat.new
-
   end
 
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
     if @flat.save
-      redirect_to root_path  
+      redirect_to flats_path  
     else
       render :new
     end
-
-
   end
 
   def edit
-
+    @flat = find_flat
   end
 
   def update
+    @flat = find_flat
+    @flat.update(flat_params)
 
+    redirect_to flats_path
   end
 
-  def delete
-
+  def destroy
+    @flat = find_flat
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
