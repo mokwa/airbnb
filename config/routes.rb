4a0c87d7139f99be24fa6d5b977d854b83dacef8
Tina Iuso <tinaiuso@gmail.com>
2021-02-23 18:24:57 +0000

    to fix the show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flats do
-    resources :bookings do
-        resources :reviews
-    end
+    resources :bookings
+  end
+  resources :bookings do
+    resources :reviews
   end
 end
