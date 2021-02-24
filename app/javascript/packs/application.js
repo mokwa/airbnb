e9fbd88aeae22105f504109e192b78a4a8abef4c
Juliana Mokwa <julimokwa@gmail.com>
2021-02-24 12:49:00 +0000

    Map pointing the locations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,12 @@ require("channels")
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// app/javascript/packs/application.js
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+  initMapbox();
+})
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
