5da02e9d13aae8372f8f230c5692f3223c2265a6
Tina Iuso <tinaiuso@gmail.com>
2021-02-25 21:50:34 +0000

    implemented search bar

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,5 @@
 class Flat < ApplicationRecord
+  belongs_to :city
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Flat.destroy_all
 User.destroy_all
+City.destroy_all
 
 juliana = User.create!(first_name: "Juliana", last_name: "Mokwa", email: "juliana@example.com", password: "123456")
-Flat.create!(name: "Tilden house", address: "London WS15PG", description: "Lovely house in west london", price: 100, user: juliana)
+london = City.create!(name: "London")
+lisbon = City.create!(name: "Lisbon")
+rome = City.create!(name: "Rome")
+paris = City.create!(name: "Paris")
+Flat.create!(name: "Tilden house", address: "London WS15PG", description: "Lovely house in west london", price: 100, user: juliana, city: london)
 
 ronaldo = User.create!(first_name: "Ronaldo", last_name: "Goncalves", email: "ronaldo@example.com", password: "123456")
-Flat.create!(name: "Italian houses", address: "Italy WS15PG", description: "Lovely house in Italy", price: 80, user: ronaldo)
+Flat.create!(name: "Italian houses", address: "Italy WS15PG", description: "Lovely house in Italy", price: 80, user: ronaldo, city: rome)
 
 tina = User.create!(first_name: "Tina", last_name: "Luso", email: "tina@example.com", password: "123456")
 
-Flat.create!(name: "house", address: "London WS15PG", description: "Lovely house in west london", price: 150, user: tina)
+Flat.create!(name: "house", address: "London WS15PG", description: "Lovely house in west london", price: 150, user: tina, city: london)
 
-Flat.create!(name: "House", address: "London WS15PG", description: "Lovely house in west london", price: 150, user: tina)
+Flat.create!(name: "House", address: "London WS15PG", description: "Lovely house in west london", price: 150, user: tina, city: london)
 
-Flat.create!(name: "Apartment", address: "Berlin WR15PG", description: "Lovely apartment in west berlin", price: 150, user: tina)
+Flat.create!(name: "Apartment", address: "Berlin WR15PG", description: "Lovely apartment in west berlin", price: 150, user: tina, city: london)
+
+Flat.create!(name: "Apartment", address: "Rue de Foo", description: "Lovely apartment in west Paris", price: 150, user: tina, city: paris)
